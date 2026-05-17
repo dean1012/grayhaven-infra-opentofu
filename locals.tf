3a74647f8882d4f582c9c68628b6156ef8105fdf
Jerry Dean Smith, Jr. <jsmith@jerry-smith.net>
2026-05-17 18:10:02 -0500

    Add base OpenTofu configuration (Refs #3)

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  client_name  = "grayhaven"
+  project_name = "core"
+
+  common_tags = [
+    "client-grayhaven",
+    "project-core",
+    "managed-by-opentofu"
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "state_encryption_passphrase" {
+  description = "Passphrase used for local OpenTofu state encryption"
+  type        = string
+  sensitive   = true
+}
+
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_key_fingerprints" {
+  description = "SSH key fingerprints authorized for droplet access"
+  type        = list(string)
+}
+
+variable "default_region" {
+  description = "DigitalOcean region"
+  type        = string
+  default     = "nyc3"
+}
+
+variable "default_os_image" {
+  description = "Operating system image"
+  type        = string
+  default     = "almalinux-10-x64"
+}
+
+variable "default_droplet_size" {
+  description = "DigitalOcean droplet size"
+  type        = string
+  default     = "s-1vcpu-1gb"
+}
+
+variable "default_vpc_cidr" {
+  description = "CIDR range for the project VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
