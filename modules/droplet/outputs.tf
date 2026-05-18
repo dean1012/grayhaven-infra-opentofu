529c1d62f4bc3ec494c9b8273109c5cab6bc9cf2
Jerry Dean Smith, Jr. <jsmith@jerry-smith.net>
2026-05-18 14:40:08 -0500

    Manage DigitalOcean project assignment (Refs #15)

--- modules/droplet/outputs.tf
+++ modules/droplet/outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = digitalocean_droplet.this.id
 }
 
+output "urn" {
+  description = "DigitalOcean droplet URN"
+  value       = digitalocean_droplet.this.urn
+}
+
 output "name" {
   description = "DigitalOcean droplet name"
   value       = digitalocean_droplet.this.name
