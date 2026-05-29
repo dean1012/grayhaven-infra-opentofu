6cc76237ff37ade6846ad76b34ce5149c318d513
Jerry Dean Smith, Jr. <jsmith@jerry-smith.net>
2026-05-29 17:00:35 -0500

    Add droplet user-data support (Refs #26)

--- modules/droplet/main.tf
+++ modules/droplet/main.tf
@@ -1,9 +1,10 @@
 resource "digitalocean_droplet" "this" {
-  name     = var.name
-  region   = var.region
-  vpc_uuid = var.vpc_id
-  ssh_keys = var.ssh_key_fingerprints
-  size     = var.size
-  image    = var.image
-  tags     = var.tags
+  name      = var.name
+  region    = var.region
+  vpc_uuid  = var.vpc_id
+  ssh_keys  = var.ssh_key_fingerprints
+  size      = var.size
+  image     = var.image
+  tags      = var.tags
+  user_data = var.user_data
 }
